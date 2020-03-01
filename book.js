5f51f9f8e34c442ac62712f7c021cbd5dc5c9335
Evan Smith <smith.m.evan@gmail.com>
2020-03-01 14:51:02 +0000

    Passing all library and book class tests

--- book.js
+++ book.js
@@ -1,7 +1,17 @@
-const Book = function (author, title, genre) {
-  this.author = author;
-  this.title = title;
-  this.genre = genre;
-};
+// const Book = function (author, title, genre) {
+//   this.author = author;
+//   this.title = title;
+//   this.genre = genre;
+// };
+//
+
+
+class Book {
+  constructor(author, title, genre) {
+    this.author = author;
+    this.title = title;
+    this.genre = genre;
+  }
+}
 
 module.exports = Book;
--- library.js
+++ library.js
@@ -1,23 +1,71 @@
-const Library = function (books = []) {
-  this.books = books;
-};
+// class Rectangle {
+//   constructor(height, width) {
+//     this.height = height;
+//     this.width = width;
+//   }
+//   // Getter
+//   get area() {
+//     return this.calcArea();
+//   }
+//   // Method
+//   calcArea() {
+//     return this.height * this.width;
+//   }
+// }
+//
+// const square = new Rectangle(10, 10);
+//
 
-Library.prototype.bookCount = function () {
-  return this.books.length;
-};
+class Library {
+  constructor(books = []){
+    this.books = books;
+  }
 
-Library.prototype.addBook = function (newBook) {
-  this.books.push(newBook);
-};
+  bookCount() {
+    return this.books.length;
+  }
 
-Library.prototype.addBooks = function (newBooks) {
-  newBooks.forEach(book => this.books.push(book));
-};
+  addBook(newBook) {
+     this.books.push(newBook);
+  }
 
-Library.prototype.printInventory = function () {
-  this.books.forEach((book) => {
-    console.log(`${book.title} by ${book.author}`);
-  });
-};
+  addBooks(newBooks) {
+     newBooks.forEach(book => this.books.push(book));
+  }
+
+  printInventory() {
+    this.books.forEach((book) => {
+        console.log(`${book.title} by ${book.author}`);
+      })
+  }
+
+
+
+}
+
+
+// console.log(square.area); // 100
+
+// const Library = function (books = []) {
+//   this.books = books;
+// };
+//
+// Library.prototype.bookCount = function () {
+//   return this.books.length;
+// };
+//
+// Library.prototype.addBook = function (newBook) {
+//   this.books.push(newBook);
+// };
+//
+// Library.prototype.addBooks = function (newBooks) {
+//   newBooks.forEach(book => this.books.push(book));
+// };
+//
+// Library.prototype.printInventory = function () {
+//   this.books.forEach((book) => {
+//     console.log(`${book.title} by ${book.author}`);
+//   });
+// };
 
 module.exports = Library;
